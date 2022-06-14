3d12b66280599beb3f6c8830f4e7b439f16ae0ba
sanjay008 <webapps008@gmail.com>
2022-06-14 15:50:47 +0530

    component removed

--- components/style.js
+++
@@ -1,8 +0,0 @@
-export default function Style() {
-	return (
-		<>
-			<link rel="stylesheet" type="text/css" href="../css/style.css" />
-			<link rel="stylesheet" type="text/css" href="../css/responsive.css" />
-		</>
-	)
-}
