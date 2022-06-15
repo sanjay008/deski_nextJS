25e62c538044ad67e3756f6067eb48962d19ecfc
sanjay008 <webapps008@gmail.com>
2022-06-15 19:23:39 +0530

    Header

--- components/header.js
+++ components/header.js
@@ -1,6 +1,7 @@
 import * as ReactDOM from "react-dom/client";
 import Navbar from './navbar'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Header() {
 	return (
@@ -8,9 +9,11 @@ export default function Header() {
 			<div className="theme-main-menu sticky-menu theme-menu-two">
 				<div className="d-flex align-items-center justify-content-center">
 					<div className="logo">
-						<a href="/">
-							<Image src="/../images/logo/deski_01.svg" alt="" width="100%" height="50%" />
-						</a>
+						<Link href="/">
+							<a>
+								<Image src="/../images/logo/deski_01.svg" alt="" width="100%" height="50%" />
+							</a>
+						</Link>
 					</div>
 					<Navbar />
 				</div>
