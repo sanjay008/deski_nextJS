8a20a9b8ef049d4d2d8b08fce7668cac3b13240d
sanjay008 <webapps008@gmail.com>
2022-06-15 21:17:58 +0530

    Home  API

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ module.exports = nextConfig
 
 module.exports = {
   images: {
-    domains: ['http://127.0.0.1:8000'],
+    domains: ['http://organickuku.com/public'],
+	imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+	deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+	disableStaticImages: false,
   },
 }
