88fa7f8ec9fd3b2b5c32a9bf65e90428a81210c4
sanjay008 <webapps008@gmail.com>
2022-06-16 19:42:23 +0530

    Contact us

--- pages/contact_us.js
+++ pages/contact_us.js
@@ -9,7 +9,7 @@ import Main from '../components/main'
 import styles from '../styles/Home.module.css'
 
 const myLoader = ({ src }) => {
-	return `http://127.0.0.1:8000/${src}`
+	return `http://organickuku.com/${src}`
 }
 
 export default function Home() {
@@ -70,40 +70,7 @@ export default function Home() {
 					</div> 
 
 					<div className="form-style-classNameic mt-200 md-mt-80">
-						<form action="#" id="contact-form" action="inc/contact.php" data-toggle="validator">
-							<div className="messages"></div>
-							<div className="row controls">
-								<div className="col-md-6" data-aos="fade-right" data-aos-duration="1200">
-									<div className="input-group-meta form-group mb-60">
-										<label>First Name</label>
-										<input type="text" placeholder="Michel" name="Fname" required="required" data-error="Name is required." />
-										<div className="help-block with-errors"></div>
-									</div>
-								</div>
-								<div className="col-md-6" data-aos="fade-left" data-aos-duration="1200">
-									<div className="input-group-meta form-group mb-60">
-										<label>Last Name</label>
-										<input type="text" placeholder="Hawkins" name="Lname" required="required" data-error="Name is required." />
-										<div className="help-block with-errors"></div>
-									</div>
-								</div>
-								<div className="col-12" data-aos="fade-up" data-aos-duration="1200">
-									<div className="input-group-meta form-group mb-60">
-										<label>Email Address</label>
-										<input type="email" placeholder="saeslal@zouj.co.uk" name="email" required="required" data-error="Valid email is required." />
-										<div className="help-block with-errors"></div>
-									</div>
-								</div>
-								<div className="col-12" data-aos="fade-up" data-aos-duration="1200">
-									<div className="input-group-meta form-group lg mb-40">
-										<label>Your Message</label>
-										<textarea placeholder="your message here.." name="message" required="required" data-error="Please,leave us a message."></textarea>
-										<div className="help-block with-errors"></div>
-									</div>
-								</div>
-								<div className="col-12" data-aos="fade-up" data-aos-duration="1200"><button className="theme-btn-two">Send Message</button></div>
-							</div>
-						</form>
+						
 					</div> 
 				</div>
 			</div> 
