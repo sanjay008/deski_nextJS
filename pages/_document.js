82494d348aad76eecc402f1cbbe77b502e782be9
sanjay008 <webapps008@gmail.com>
2022-06-16 19:19:11 +0530

    script

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { hydrate, render } from "react-dom"
-
-
-
+import Script from 'next/script'
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
