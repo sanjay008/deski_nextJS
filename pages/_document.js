5d67a15000a6bd0abaf63f1c3c7ce25f43b785e0
sanjay008 <webapps008@gmail.com>
2022-06-16 19:17:19 +0530

    js changes

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,17 @@ class MyDocument extends Document {
       <Html>
         <Head>		  
           <NextScript />
+			<Script src="../vendor/jquery.min.js" strategy="beforeInteractive" />
+			<Script src="../vendor/popper.js/popper.min.js" strategy="beforeInteractive" />
+			<Script src="../vendor/bootstrap/js/bootstrap.min.js" strategy="beforeInteractive" />
+			<Script src="../vendor/mega-menu/assets/js/custom.js" strategy="beforeInteractive" />
+			<Script src="../vendor/aos-next/dist/aos.js" strategy="beforeInteractive" />
+			<Script src="../vendor/jquery.appear.js" strategy="afterInteractive" />
+			<Script src="../vendor/jquery.countTo.js" strategy="afterInteractive" />
+			<Script src="../vendor/slick/slick.min.js" strategy="afterInteractive" />
+			<Script src="../vendor/fancybox/dist/jquery.fancybox.min.js" strategy="afterInteractive" />
+			<Script src="../js/theme.js" strategy="afterInteractive" />
+			<Script src="../assets/js/custom.js" strategy="afterInteractive" />
         </Head>
         <body>
 			<div className="main-page-wrapper">
