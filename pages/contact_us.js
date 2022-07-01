135e45af34c279a882603cc7a8f8b77b5794834d
sanjay008 <webapps008@gmail.com>
2022-07-01 20:22:28 +0530

    contact

--- pages/contact_us.js
+++ pages/contact_us.js
@@ -100,40 +100,7 @@ const Contact = () => {
 					</div> 
 
 					<div className="form-style-classic mt-200 md-mt-80">
-						<form action="#" id="contact-form" action="inc/contact.php" data-toggle="validator">
-							<div className="messages"></div>
-							<div className="row controls">
-								<div className="col-md-6" data-aos="fade-right" data-aos-duration="1200">
-									<div className="input-group-meta form-group mb-60">
-										<label>First Name</label>
-										<input type="text" placeholder="Michel" name="Fname" required data-error="Name is required." />
-										<div className="help-block with-errors"></div>
-									</div>
-								</div>
-								<div className="col-md-6" data-aos="fade-left" data-aos-duration="1200">
-									<div className="input-group-meta form-group mb-60">
-										<label>Last Name</label>
-										<input type="text" placeholder="Hawkins" name="Lname" required data-error="Name is required." />
-										<div className="help-block with-errors"></div>
-									</div>
-								</div>
-								<div className="col-12" data-aos="fade-up" data-aos-duration="1200">
-									<div className="input-group-meta form-group mb-60">
-										<label>Email Address</label>
-										<input type="email" placeholder="saeslal@zouj.co.uk" name="email" required data-error="Valid email is required." />
-										<div className="help-block with-errors"></div>
-									</div>
-								</div>
-								<div className="col-12" data-aos="fade-up" data-aos-duration="1200">
-									<div className="input-group-meta form-group lg mb-40">
-										<label>Your Message</label>
-										<textarea placeholder="your message here.." name="message" required data-error="Please,leave us a message."></textarea>
-										<div className="help-block with-errors"></div>
-									</div>
-								</div>
-								<div className="col-12" data-aos="fade-up" data-aos-duration="1200"><button className="theme-btn-two">Send Message</button></div>
-							</div>
-						</form>
+						
 					</div> 
 				</div>
 			</div> 
